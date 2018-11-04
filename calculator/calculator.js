96a7c1d6751f118fc09ee8425160894cb8a43d1e
Jeff Vergne <vergnejf@gmail.com>
2018-11-04 08:36:40 +0100

    completion of the calculator exercise

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -2,24 +2,37 @@ function add (a,b) {
 	return a+b;
 }
 
+console.log(add(12,65))
+
 function subtract (a,b) {
 	return a-b;
 }
 
-function sum () {
+//computes the sum of the elements of an array
+function sum (array) {
 	
+	return array.reduce(((total, currentValue) => total + currentValue),0);
+
 }
 
-function multiply () {
-	
+//computes the multiplication of the elements of an array
+function multiply (array) {
+	return array.reduce(((total, currentValue) => total * currentValue),1);
 }
 
-function power() {
-	
+//should return the result of number1 power number 2
+function power(number1,number2) {
+	return Math.pow(number1,number2);
 }
 
-function factorial() {
-	
+function factorial(input) {
+	if (!input) return 1;
+
+	let result=1;
+	for (i=input;i>0;i--){
+		result*=i;
+	}
+	return result;
 }
 
 module.exports = {
