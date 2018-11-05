543f7a603aa41e5dd14502d5203a1c9589ecce28
Jeff Vergne <vergnejf@gmail.com>
2018-11-05 10:54:45 +0100

    completio of the palindroms exercise

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function() {
+const palindromes = function(stringToCheck) {
 
+    //converting into lower case
+    stringToCheck=stringToCheck.toLowerCase().replace(/[^a-z]/g,"");
+    console.log('converted string is ' + stringToCheck);
+
+    let tempo = stringToCheck.split(''); //separates all the characters which all become the elements of an array
+    tempo = tempo.reverse(); //reversing the order of the elements of the array
+    tempo = tempo.join('');  //reuniting into one single string
+    
+    return tempo==stringToCheck;
 }
 
 module.exports = palindromes
