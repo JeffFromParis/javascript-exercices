b52987f2f57d552ad8f69d74a3a24cdf09c29a06
Jeff Vergne <vergnejf@gmail.com>
2018-11-07 12:14:05 +0100

    completing fibonacci exercise

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,30 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
 
+    if (number<1) return "OOPS";
+
+    if (typeof(number)=='string'){
+        if (number.charCodeAt()>=48 && number.charCodeAt()<=57){
+            number=parseInt(number);
+            console.log('String has been converted to number');
+        }else{
+            return "The argument is not a number";
+        }
+    }
+
+    fiboArray=new Array(number);
+    
+    for(var i=0;i<number;i++){
+        fiboArray[i]=computeFibo(i,fiboArray);
+    }
+
+    // console.log('initial fiboArray is ' + fiboArray);
+    // const resultArray=fiboArray.map(x => computeFibo(x,fiboArray));
+    // console.log('fibonacci suite is ' + resultArray);
+    return fiboArray.pop();
+}
+
+const computeFibo = function (num, fiboArray){
+    return a = (num<2) ? 1 : (fiboArray[num-1] + fiboArray[num-2]);
 }
 
 module.exports = fibonacci
